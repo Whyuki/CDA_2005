49e077014566b70b9aa9388f90c7f3b43b5dc055
Whyuki <whyuki@outlook.fr>
2020-05-11 15:33:51 +0200

    exo pendu limite saisie user 1 char

--- FOAD_Algo/src/com/crm/algo05/Ex3_6.java
+++ FOAD_Algo/src/com/crm/algo05/Ex3_6.java
@@ -16,6 +16,8 @@ public class Ex3_6 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String mot = "";
+		String saisie = "";
+		char lettre =' ';
 		boolean present = false;
 		int tentative = 6;
 		boolean end = false;
@@ -35,37 +37,44 @@ public class Ex3_6 {
 
 				System.out.println("\n" + "Joueur 2 : Saisir une lettre");
 
-				char lettre = sc.next().charAt(0);
+				saisie = sc.next();
 
-				for (int i = 0; i < devine.length; i++) {
-
-					if (mot.charAt(i) == lettre) {
-						devine[i] = lettre;
-						present = true;
-					}
-
-				}
-				if (present == true) {
-					System.out.println("Lettre présente");
+				if (saisie.length() > 1) {
+					System.out.println("UNE lettre, pas plus ;)");
 				} else {
-					System.out.println("Lettre non présente");
-					tentative--;
-					System.out.println(tentative + " tentative(s) restante(s)");
-				}
-				for (int i = 0; i < devine.length; i++) {
-					System.out.print(devine[i] + " ");
-				}
-				System.out.println();
 
-				for (int i = 0; i < devine.length; i++) {
-					if (devine[i] == '_') {
-						end = false;
-						break;
-					} else {
-						end = true;
+					lettre = saisie.charAt(0);
+
+					for (int i = 0; i < devine.length; i++) {
+
+						if (mot.charAt(i) == lettre) {
+							devine[i] = lettre;
+							present = true;
+						}
+
 					}
+					if (present == true) {
+						System.out.println("Lettre présente");
+					} else {
+						System.out.println("Lettre non présente");
+						tentative--;
+						System.out.println(tentative + " tentative(s) restante(s)");
+					}
+					for (int i = 0; i < devine.length; i++) {
+						System.out.print(devine[i] + " ");
+					}
+					System.out.println();
+
+					for (int i = 0; i < devine.length; i++) {
+						if (devine[i] == '_') {
+							end = false;
+							break;
+						} else {
+							end = true;
+						}
+					}
+					present = false;
 				}
-				present = false;
 			}
 
 			if (tentative == 0 || end == true) {
