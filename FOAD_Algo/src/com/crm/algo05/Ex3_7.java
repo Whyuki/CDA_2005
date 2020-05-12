f0f07bcf8cbe8270359429890f1f59916ff1cf3c
Whyuki <whyuki@outlook.fr>
2020-05-12 21:48:27 +0200

    ex 3.7 dichotomie string

---
+++ FOAD_Algo/src/com/crm/algo05/Ex3_7.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.crm.algo05;
+
+import java.util.Scanner;
+
+/**
+ * Recherche par dichotomie d'un élément dans un tableau classé
+ * 
+ * @author Joanna
+ *
+ */
+public class Ex3_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] tableau = { "agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie" };
+
+		String prenom = "";
+
+		int min = 0, max = tableau.length - 1;
+		int mid = (min + max) / 2;
+		boolean present = false;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Saisir un prénom");
+		prenom = sc.nextLine();
+
+		while (present == false && min < max) {
+
+			if (prenom.compareTo(tableau[mid]) == 0) { // si trouvé :
+				present = true;
+				break;
+
+			} else if (prenom.compareTo(tableau[mid]) < 0) { // si avant
+				max = mid - 1;
+
+			} else { // si après
+				min = mid + 1;
+			}
+			mid = (min + max) / 2;
+		}
+
+		if (prenom.equals(tableau[mid])) {
+			System.out.println("Prénom trouvé à la case n°" + (mid + 1));
+		} else {
+			System.out.println("Prénom non trouvé");
+		}
+		sc.close();
+	}
+
+}
