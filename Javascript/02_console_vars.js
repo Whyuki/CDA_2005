cbe4ea2b3234f9363470655a4a44eceec5bf9c5c
Whyuki <whyuki@outlook.fr>
2020-05-14 15:34:19 +0200

    cours js node 14 mai

---
+++ Javascript/02_console_vars.js
@@ -0,0 +1,14 @@
+
+{
+
+    let entier = 1;
+    
+    const constante = "HOP !";
+    
+    var chaine = "Hello world !";
+    console.log(entier);
+}
+
+console.log(chaine);
+// console.log(entier);
+    
