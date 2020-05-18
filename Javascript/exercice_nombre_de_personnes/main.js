45700dbb282b122f091c1f85ee086735ed41f4ec
Whyuki <whyuki@outlook.fr>
2020-05-18 17:08:58 +0200

    2.7

---
+++ Javascript/exercice_nombre_de_personnes/main.js
@@ -0,0 +1,52 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+{
+  let lecteur = function () {
+    rl.question("\n" + "Saisir un âge :" + "\n", (answer) => {
+      age = answer;
+
+      if (age < 20) {
+        compteurJeune += 1;
+      } else if (age == 20) {
+        compteur += 1;
+      } else {
+        compteurNonJeune += 1;
+      }
+
+      if (i < 19) {
+        lecteur();
+        i += 1;
+      } else {
+        if (compteur == 0 && compteurJeune == 0) {
+          console.log("Il n'y a que des vieux, pardon, non jeunes *");
+        } else if (compteurNonJeune == 0 && compteurJeune == 0) {
+          console.log("Il n'y a que des personnes de 20ans");
+        } else if (compteur == 0 && compteurNonJeune == 0) {
+          console.log("Il n'y a que des jeunes*");
+        } else {
+          console.log("Il y a " + compteur + " personnes de 20ans");
+          console.log(
+            "Il y a " + compteurJeune + " personnes de moins de 20ans"
+          );
+          console.log(
+            "Il y a " + compteurNonJeune + " personnes de plus de 20ans"
+          );
+        }
+        rl.close();
+      }
+    });
+  };
+  let i = 0;
+  let compteur = 0;
+  let compteurJeune = 0;
+  let compteurNonJeune = 0;
+
+  let age = 0;
+
+  lecteur();
+}
