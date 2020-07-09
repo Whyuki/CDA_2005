72d93d98e31268f3778f9aeef4a429e9363c36b8
Joanna <51273476+Whyuki@users.noreply.github.com>
2020-07-09 21:00:21 +0200

    intro Visual Studio

---
+++ FOAD_C#/CDA2005_VS_Intro/CDA2005_VS_Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CDA2005_VS_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
