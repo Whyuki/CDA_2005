726b3b9bddbf6cbc3db3f0162899e8baff783478
Joanna <51273476+Whyuki@users.noreply.github.com>
2020-10-02 14:23:24 +0200

    tp_agence_voyages exercice prog

--- FOAD_MERISE/tp_agence_voyages/00_exercices_prog_sql.sql
+++ FOAD_MERISE/tp_agence_voyages/00_exercices_prog_sql.sql
@@ -3,12 +3,37 @@ use tp_agence_voyages;
 
 -- Créer les procédures et/ou déclencheurs répondant aux cas suivants :
 
-
-
 -- 1) Modifier les informations d'un client :
 -- 		Le mot de passe ainsi que la date d'ajout du client ne peuvent pas être modifiés sur cette procédure
 
+DROP PROCEDURE IF EXISTS update_client;
 
+DELIMITER $$
+
+CREATE PROCEDURE update_client
+(
+	IN client_id_in INT,
+	IN client_lastname_in VARCHAR(32),
+    IN client_firstname_in VARCHAR(32),
+    IN client_email_in VARCHAR(128),
+    IN client_phone_in CHAR(16)
+)
+BEGIN 
+
+	UPDATE clients
+	SET client_lastname = client_lastname_in,
+    client_firstname = client_firstname_in,
+    client_email = client_email_in,
+    client_phone = client_phone_in
+	WHERE client_id=client_id_in;
+    
+END$$
+
+DELIMITER ;
+
+-- test
+
+CALL update_client(1,'jean','bon','jeanbon@nomnomnom.fr','');
 
 -- 2) Modifier le mot de passe d'un client (sans chiffrement pour l'exercice)
 -- 		La procédure accepte 3 paramètres :  Le mot de passe actuel | Le nouveau mot de passe | Le nouveau mot de passe (pour confirmation)
