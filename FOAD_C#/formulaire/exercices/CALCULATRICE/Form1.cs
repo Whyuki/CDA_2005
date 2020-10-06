db1567de3ef548ab1cc81f78dfa60525f3001a63
Whyuki <whyuki@outlook.fr>
2020-10-06 10:29:10 +0200

    winform exercice calculatrice v2

--- FOAD_C#/formulaire/exercices/CALCULATRICE/Form1.cs
+++ FOAD_C#/formulaire/exercices/CALCULATRICE/Form1.cs
@@ -19,70 +19,12 @@ namespace CALCULATRICE
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void buttonNumber_Click(object sender, EventArgs e)
         {
-
-        }
-
-
-        private void button0_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "0+";
-            this.somme += 0;
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "1+";
-            this.somme += 1;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "2+";
-            this.somme += 2;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "3+";
-            this.somme += 3;
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "4+";
-            this.somme += 4;
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "5+";
-            this.somme += 5;
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "6+";
-            this.somme += 6;
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "7+";
-            this.somme += 7;
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "8+";
-            this.somme += 8;
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            this.textBoxAffichageCalcul.Text += "9+";
-            this.somme += 9;
+            Button buttonNumber = (Button)sender;
+            int buttonValue = Int32.Parse(buttonNumber.Text.ToString());
+            this.textBoxAffichageCalcul.Text += buttonValue + "+";
+            this.somme += buttonValue ;
         }
 
         private void vider_Click(object sender, EventArgs e)
