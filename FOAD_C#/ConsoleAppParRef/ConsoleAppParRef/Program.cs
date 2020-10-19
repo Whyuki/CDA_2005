0d04b25ae9f3cfbe4f06a65cb81571cfec8cde07
Whyuki <whyuki@outlook.fr>
2020-10-19 11:31:09 +0200

    exemple parametre out

---
+++ FOAD_C#/ConsoleAppParRef/ConsoleAppParRef/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppParRef
+{
+    class Program
+    {
+
+        static double AddSous(double d1, double d2, out double sous)
+        {
+            sous = d1 - d2;
+            return d1 + d2;
+
+        }
+
+        static void Main(string[] args)
+        {
+            double d1 = 1.5;
+            double d2 = 2.5;
+            double resultSous;
+            double resultAdd;
+
+            resultAdd = AddSous(d1, d2, out resultSous);
+
+        }
+    }
+}
