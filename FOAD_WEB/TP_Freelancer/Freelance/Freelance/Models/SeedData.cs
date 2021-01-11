52442193f4150bafb51643ddb28a350647a6e579
Whyuki <whyuki@outlook.fr>
2021-01-11 19:05:16 +0100

    TP freelancer seedData +html/css

---
+++ FOAD_WEB/TP_Freelancer/Freelance/Freelance/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Freelance.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freelance.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcClientContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcClientContext>>()))
+            {
+                // Look for any movies.
+                if (context.Client.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Client.AddRange(
+                    new Client
+                    {
+                        Nom = "Meowstache",
+                        Email = "stashy@mail.com"
+
+                    },
+
+                    new Client
+                    {
+                        Nom = "Nyupont",
+                        Email = "m.nyu@mail.com"
+                    },
+
+                    new Client
+                    {
+                        Nom = "Nyoe",
+                        Email = "john.nyoe@mail.com"
+                    },
+
+                    new Client
+                    {
+                        Nom = "Chatouillette",
+                        Email = "touillette@mail.com"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
